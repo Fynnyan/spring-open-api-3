c9075222ef92055ff1597ba5924b3587c0c7bc9f
Fynnian <you@example.com>
2020-08-04 06:03:25 +0200

    enabled the build info param from the spring boot maven plugin and changed the sortion of operations according to method

--- src/main/kotlin/com/fynnian/springopenapi3/configuration/OpenApiConfiguration.kt
+++ src/main/kotlin/com/fynnian/springopenapi3/configuration/OpenApiConfiguration.kt
@@ -3,18 +3,20 @@ package com.fynnian.springopenapi3.configuration
 import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.info.Info
 import io.swagger.v3.oas.models.info.License
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.info.BuildProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-class OpenApiConfiguration {
+class OpenApiConfiguration @Autowired constructor(private val buildProperties: BuildProperties) {
 
   @Bean
   fun openApiConfig(): OpenAPI {
     return OpenAPI()
         .info(Info().title("Test API")
                     .description("Demo to showcase the springdoc usage")
-                    .version("1")
+                    .version(buildProperties.version)
                     .license(License().name("MIT")))
   }
 }
