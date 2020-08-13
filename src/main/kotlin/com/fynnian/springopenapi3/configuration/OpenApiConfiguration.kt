223998a55185444711a4c08fa8ad67e644f3450d
Fynnian <you@example.com>
2020-08-13 22:24:22 +0200

    added a example on how to add a global header, usable for all swagger components / objects

--- src/main/kotlin/com/fynnian/springopenapi3/configuration/OpenApiConfiguration.kt
+++ src/main/kotlin/com/fynnian/springopenapi3/configuration/OpenApiConfiguration.kt
@@ -1,12 +1,18 @@
 package com.fynnian.springopenapi3.configuration
 
+import io.swagger.v3.oas.models.Components
 import io.swagger.v3.oas.models.OpenAPI
+import io.swagger.v3.oas.models.headers.Header
 import io.swagger.v3.oas.models.info.Info
 import io.swagger.v3.oas.models.info.License
+import io.swagger.v3.oas.models.media.StringSchema
+import io.swagger.v3.oas.models.parameters.HeaderParameter
+import org.springdoc.core.customizers.OpenApiCustomiser
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.info.BuildProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+
 
 @Configuration
 class OpenApiConfiguration @Autowired constructor(private val buildProperties: BuildProperties) {
@@ -18,5 +24,36 @@ class OpenApiConfiguration @Autowired constructor(private val buildProperties: B
                     .description("Demo to showcase the springdoc usage")
                     .version(buildProperties.version)
                     .license(License().name("MIT")))
+        .components(components)
   }
+
+  /**
+   * Configuration bean, adding a global header for ever path and every operation.
+   * Can be used to do other nifty stuff on a global level without the need to annotate every class and request mapping
+   */
+  @Bean
+  fun addGlobalHeader(): OpenApiCustomiser {
+    return OpenApiCustomiser { openApi ->
+      openApi.paths.values
+          .flatMap { pathItem -> pathItem.readOperations() }
+          .forEach { operation -> operation.addParametersItem(HeaderParameter().`$ref`(ApiRefs.X_SUPER_COOL_HEADER)) }
+    }
+  }
+
+  /**
+   * List of components that are globally available, can and need to be referenced if the want to be used someplace.
+   * Use the reference string on the refs field of the various annotations.
+   *
+   * example: #/components/headers/x-super-cool-global-header
+   */
+  val components: Components = Components()
+      .addHeaders("x-super-cool-global-header",
+                  Header().example("cool value")
+                          .description("Global header that is required for every request (not needed for the requests of the demo to work)")
+                          .required(false)
+                          .schema(StringSchema()))
+}
+
+object ApiRefs {
+  const val X_SUPER_COOL_HEADER = "#/components/headers/x-super-cool-global-header"
 }
